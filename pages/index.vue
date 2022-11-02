8ad6e470648819b202abdb610c0e4646d6483615
搋兔 <naruto6404@hotmail.com>
2022-11-02 22:21:06 +0800

    update: add a simple design for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  
+  <div class="w-full h-[3000px]">
+    <img class="w-full aspect-video object-cover" src="/images/cover.jpg">
+    <div class="">
+      Index
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  layout: 'LayoutMain',
-  head () {
-    return {
-      
-    }
-  }
+  layout: 'LayoutMain'
 }
 </script>
