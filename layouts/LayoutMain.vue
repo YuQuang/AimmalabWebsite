d7730fb16d35e84d2c0afecc93d9b6c447ed5bec
搋兔 <naruto6404@hotmail.com>
2022-11-02 22:21:06 +0800

    update: add a simple design for index

--- layouts/LayoutMain.vue
+++ layouts/LayoutMain.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <div>Headers</div>
-    <Nuxt />
-    <div>Footer</div>
+  <div class="w-screen h-screen overflow-hidden">
+    <NavBar target="main" />
+    <main id="main" class="overflow-y-auto w-full h-full flex flex-col">
+      <div class="flex-1">
+        <Nuxt />
+      </div>
+      <div class="flex-none">Footer</div>
+    </main>
+    <BackTopButton target="main" />
   </div>
 </template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+import BackTopButton from '../components/BackTopButton.vue'
+
+export default {
+  components: { NavBar, BackTopButton }
+}
+</script>
