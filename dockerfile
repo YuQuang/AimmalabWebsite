69c385b8972fd85ea4c6e7a3e69e2b011b0a6d9a
Roy.Xu <bbb233456@gmail.com>
2023-01-07 18:05:27 +0800

    Migrate from nuxt2 to nuxt3

--- dockerfile
+++ dockerfile
@@ -9,9 +9,6 @@ ENV NODE_OPTIONS=--openssl-legacy-provider
 
 # Install dependency
 RUN npm install
-
-RUN npm install loader-utils@2.0.3
-
 RUN npm run build
 
 # Port expose
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <NavBar target="main" />
     <main id="main" class="overflow-y-auto w-full h-full flex flex-col">
       <div class="flex-1">
-        <Nuxt />
+        <NuxtPage />
       </div>
       <div class="flex-none">
         <MainFooter />
