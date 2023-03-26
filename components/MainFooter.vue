f8822db55a814a3553f6a77bf213f37ed40acbb7
Roy.Xu <bbb233456@gmail.com>
2023-03-26 22:20:32 +0800

    Fix footer UI bug

--- components/MainFooter.vue
+++ components/MainFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-cyan-900 flex justify-between">
-    <div class="text-slate-100 text-sm gap-3 grid grid-cols-1 p-10">
+  <div class="bg-cyan-900 grid md:grid-cols-2 grid-cols-1 gap-4">
+    <div class="text-slate-100 text-sm gap-3 grid grid-cols-1 md:p-10 p-3">
       <div>
         電話：(04)23924505#8701,#8702,#8703,#8706
       </div>
@@ -11,11 +11,8 @@
         地址：411030臺中市太平區坪林里中山路二段57號
       </div>
     </div>
-    <div />
-    <div class="p-20 text-slate-100 flex">
-      <span class="self-end">
-        @NCUT Aimmalab designed by RoyXu
-      </span>
+    <div class="md:p-20 py-4 md:text-left text-center text-slate-100">
+      @NCUT Aimmalab designed by RoyXu
     </div>
   </div>
 </template>
