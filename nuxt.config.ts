47282ce14355a974c2a013af890b87da51aa87a1
Roy.Xu <bbb233456@gmail.com>
2023-11-23 04:02:17 +0800

    Add nuxt-security package to improve security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        'nuxt-security',
+    ],
     typescript: {
         typeCheck: true
-    }
+    },
+    // Nuxt-security Settings
+    // Reference: https://nuxt-security.vercel.app/
+    security: {
+        headers: {
+            contentSecurityPolicy: "img-src 'self' https:;",
+            crossOriginEmbedderPolicy: false,
+        },
+    },
 })
