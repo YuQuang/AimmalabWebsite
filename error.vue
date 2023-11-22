873c5785b633afb1ee773b935e0ac6673e8d01b1
Roy.Xu <bbb233456@gmail.com>
2023-11-23 03:26:21 +0800

    Fix XSS attack

---
+++ error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NuxtLayout name="404">
+      <div>
+        <div class="text-4xl">You've Arrived Here on Error, boss</div>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+    }
+  }
+</script>
+
+<style>
+  .button {
+    padding: 4px 6px;
+    margin: 10px 0px;
+    background: black;
+    color: white;
+  }
+</style>
